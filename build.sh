15a10ff314f0db15b1613cb3767e91e318235b4e
xuriqingyun <zhouxu@pioneerservice.cn>
2019-06-05 17:26:47 +0800

    mq 配置

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-docker rm -vf docker-env
-docker rmi zhouxu/docker-env:v0.2
-docker build -t zhouxu/docker-env:v0.2 .
-echo "container create ok"
---
+++ src/main/java/zhouxu/site/dockerenv/command/CommandListener.java
@@ -0,0 +1,10 @@
+package zhouxu.site.dockerenv.command;
+
+/**
+ * 接收mq消息
+ *
+ * @author zhouxu
+ * @date 2019/06/05 15:06
+ **/
+public class CommandListener {
+} 
