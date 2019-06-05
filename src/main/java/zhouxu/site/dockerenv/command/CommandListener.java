f0bcce45a3e8ce3d49f88f2fe30d52e27b6ad897
xuriqingyun <zhouxu@pioneerservice.cn>
2019-06-05 17:28:27 +0800

    mq配置调整

--- src/main/java/zhouxu/site/dockerenv/command/CommandListener.java
+++ src/main/java/zhouxu/site/dockerenv/command/CommandListener.java
@@ -1,10 +1,31 @@
 package zhouxu.site.dockerenv.command;
 
+import com.rabbitmq.client.Channel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
 /**
  * 接收mq消息
  *
  * @author zhouxu
  * @date 2019/06/05 15:06
  **/
+@Component
+@Slf4j
 public class CommandListener {
+
+    @RabbitListener(queues = "msg-queue")
+//    @RabbitListener(queuesToDeclare = )
+    public void onMessage(String msg, Channel channel, Message message) {
+        try {
+            log.info("msg-queue revice msg={}",msg);
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 } 
--- src/test/java/zhouxu/site/dockerenv/command/CommandListenerTest.java
+++ src/test/java/zhouxu/site/dockerenv/command/CommandListenerTest.java
@@ -1,5 +1,21 @@
+package zhouxu.site.dockerenv.command;
+
+import org.junit.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import zhouxu.site.dockerenv.DockerEnvApplicationTests;
+
 import static org.junit.Assert.*;
 
-public class CommandListenerTest {
+@Component
+public class CommandListenerTest extends DockerEnvApplicationTests {
 
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Test
+    public void process() {
+        rabbitTemplate.convertAndSend("msg-queue","hello");
+    }
 }
